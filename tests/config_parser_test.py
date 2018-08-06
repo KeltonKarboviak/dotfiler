aec66306faaff8055370688db3a455e7e8bb1a28
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-08-05 22:22:56 -0500

    WIP, add dotfile config parser tests

---
+++ tests/config_parser_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from io import StringIO
+from unittest import TestCase
+
+
+DotfileConfigParserTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_config_must_have_version(self):
+        pass
+
