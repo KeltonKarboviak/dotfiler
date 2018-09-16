e29e74ee27538e1684768140b0c8beb19882b165
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-09-16 17:17:47 -0500

    Format test.py

--- test.py
+++ test.py
@@ -24,29 +24,19 @@ x2 = {
     'yours',
     'blah',
     'blah',
-    'blah'
+    'blah',
 }
 y = (1, 2, 3)
 z = (1, 2, 3)
-z2 = (
-    1,
-    2,
-    3,
-)
+z2 = (1, 2, 3)
 
 foo = 'This is a really long string xxxxxxxxxxxxx: {}, {}, {}, {}.....'.format(
-    a,
-    b,
-    c,
-    d
+    a, b, c, d
 )
 
 
 def long_function_name_that_is_so_long_to_type_xxxxxxxxxxxx(
-    param1,
-    param2,
-    param3,
-    param4
+    param1, param2, param3, param4
 ):
     pass
 
@@ -57,11 +47,11 @@ def long_function_name_that_is_so_long_to_type_xxxxxxxxxxxx(
 
 
 class MyClass(object):
-
     def __init__(self, x):
         pass
 
-    def method_1():        return 1 + 2
+    def method_1():
+        return 1 + 2
 
 
 d = {
@@ -72,11 +62,11 @@ d = {
     'k5': 'v5',
     'k6': 'v6',
     'k7': 'v7',
-    'k8': 'v8'
+    'k8': 'v8',
 }
 
-l1 = [i**2 for i in range(100)]
-l2 = [i**2 + 10 * 3 / 2 * 105 for i, ii in enumerate(range(100)) if i > 30]
+l1 = [i ** 2 for i in range(100)]
+l2 = [i ** 2 + 10 * 3 / 2 * 105 for i, ii in enumerate(range(100)) if i > 30]
 l3 = [['ttt'] * i for i in range(5) for t in range(5)]
 
 
@@ -84,53 +74,20 @@ def func(x, y, z):
     return x + y + z
 
 
-func(1, 2, z = 5)
-long_function_name_that_is_so_long_to_type_xxxxxxxxxxxx(param1,param2,param3='blah2',param4='blah3',)
+func(1, 2, z=5)
 long_function_name_that_is_so_long_to_type_xxxxxxxxxxxx(
-    param1,
-    param2,
-    param3='blah2',
-    param4='blah3'
+    param1, param2, param3='blah2', param4='blah3'
+)
+long_function_name_that_is_so_long_to_type_xxxxxxxxxxxx(
+    param1, param2, param3='blah2', param4='blah3'
 )
 
-func({
-    'key1': 'value1',
-    'key2': 'value2',
-    'key3': 'value3',
-})
+func({'key1': 'value1', 'key2': 'value2', 'key3': 'value3'})
 
-func({
-    1,
-    2,
-    3,
-},
-     {
-         1,
-         2,
-         3,
-     },
-     {
-         1,
-         2,
-         3,
-     })
+func({1, 2, 3}, {1, 2, 3}, {1, 2, 3})
 
-temp = [
-    {
-        1,
-        2,
-        3,
-    },
-     {
-         1,
-         2,
-         3,
-     },
-     {
-         1,
-         2,
-         3,
-     }
-]
+temp = [{1, 2, 3}, {1, 2, 3}, {1, 2, 3}]
 
-really_long_variable_name_that_doesnt_make_much_sense = 1000 * 500 / 2 + 1 + 1 * 2000 / 1 / 1000
+really_long_variable_name_that_doesnt_make_much_sense = (
+    1000 * 500 / 2 + 1 + 1 * 2000 / 1 / 1000
+)
